9d1a86129926c6bf80fc63941cc9ae61753aa00d
Alex Simisi Nta <alexsms.devops@gmail.com>
2022-12-12 02:29:21 +0100

    remove terraform version

--- format.tf
+++ format.tf
@@ -1,15 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~>3.0"
-    }
-  }
-}
-
-provider "aws" {
-  
-}
+provider "aws" {}
 
 resource "aws_instance" "myEC2" {
   ami           = "ami-0a3eea5b2e98895da"
