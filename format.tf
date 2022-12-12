ce2edb25d5b5cd0aed3b759e3aa161823bf7eaa1
Alex Simisi Nta <alexsms.devops@gmail.com>
2022-12-12 02:21:58 +0100

    remove element in provider

--- format.tf
+++ format.tf
@@ -8,9 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIAT7NUNHQOQPJEHXEF"
-  secret_key = "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"
+  
 }
 
 resource "aws_instance" "myEC2" {
